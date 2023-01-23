9f122fc637f4103e9b147e0228f4e23fcef44e2c
MUHESI <mvmmuhesi@gmail.com>
2023-01-23 12:40:28 +0200

    some changes

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -75,7 +75,7 @@ const OurClasses = ({ setSelectedPage }: Props) => {
             </p>
           </div>
         </motion.div>
-        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+        <div className="mainScrollbar mt-10  w-full overflow-x-auto overflow-y-hidden">
           <ul className="w-[2800px] whitespace-nowrap">
             {classes.map((item: ClassType, index) => (
               <Class
